29cbd4f46737133593874b222c9118c01c86c299
summarek <robert.skrzyniecki@gmail.com>
2020-05-01 23:39:15 +0200

    Delete unused liberies

--- cos.js
+++
@@ -1,8 +0,0 @@
-function Person(name, age, hairColor) {
-    this.name = name;
-    this.age = age;
-    this.hairColor = hairColor;
-}
-Person.prototype.sayName = function() {
-    console.log("my name is " + this.name);
-};
